4a4a8af7bf299600b76c39128deb11b86ef63c02
jiyonggg <kimvergognoso21@hufs.ac.kr>
2026-01-31 22:32:29 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/whatshu/whatshu_be/WhatshuBeApplication.java
@@ -0,0 +1,13 @@
+package com.whatshu.whatshu_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatshuBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatshuBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whatshu/whatshu_be/WhatshuBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatshu.whatshu_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatshuBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
