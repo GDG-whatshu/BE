566e84e58269ce91ccf12badc577690a185f010e
jiyonggg <kimvergognoso21@hufs.ac.kr>
2026-02-04 12:29:36 +0900

    feat: 기수 생성 엔드포인트 (POST /cohorts)

--- src/main/java/com/whatshu/whatshu_be/membership/controller/CohortController.java
+++ src/main/java/com/whatshu/whatshu_be/membership/controller/CohortController.java
@@ -1,9 +1,11 @@
 package com.whatshu.whatshu_be.membership.controller;
 
+import com.whatshu.whatshu_be.membership.dto.CohortRequestDto;
 import com.whatshu.whatshu_be.membership.dto.CohortResponseDto;
 import com.whatshu.whatshu_be.membership.service.CohortService;
 import com.whatshu.whatshu_be.global.common.CommonResponseBody;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,5 +23,14 @@ public class CohortController {
         List<CohortResponseDto> data = cohortService.getAllCohorts();
 
         return ResponseEntity.ok(CommonResponseBody.success(data));
+    }
+
+    @PostMapping
+    public ResponseEntity<CommonResponseBody<CohortResponseDto>> createCohort(@RequestBody CohortRequestDto cohortRequestDto) {
+        CohortResponseDto data = cohortService.createCohort(cohortRequestDto);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(CommonResponseBody.success(data));
     }
 }
