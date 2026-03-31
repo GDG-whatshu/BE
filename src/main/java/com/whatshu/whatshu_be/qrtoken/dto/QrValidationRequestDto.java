66c181342430f7438a777e2bfeb0e6b060762ead
jiyonggg <kimvergognoso21@hufs.ac.kr>
2026-03-31 09:12:29 +0900

    feat: QR 토큰 검증 요청 객체 유효성 검사

--- src/main/java/com/whatshu/whatshu_be/qrtoken/dto/QrValidationRequestDto.java
+++ src/main/java/com/whatshu/whatshu_be/qrtoken/dto/QrValidationRequestDto.java
@@ -1,5 +1,6 @@
 package com.whatshu.whatshu_be.qrtoken.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,5 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class QrValidationRequestDto {
 
+    @NotBlank(message = "토큰 정보는 필수입니다.")
     private String token;
 }
