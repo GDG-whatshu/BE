de51a7b31277ee6815f8e4e5acfeb8673d03a8c6
jiyonggg <kimvergognoso21@hufs.ac.kr>
2026-03-31 11:17:51 +0900

    refactor(crypto): 인터페이스 메소드 접근 제어자 제거

--- src/main/java/com/whatshu/whatshu_be/global/util/CryptoProvider.java
+++ src/main/java/com/whatshu/whatshu_be/global/util/CryptoProvider.java
@@ -2,6 +2,6 @@ package com.whatshu.whatshu_be.global.util;
 
 public interface CryptoProvider {
 
-    public String encode(Object data);
-    public Object decode(String token);
+    String encode(Object data);
+    Object decode(String token);
 }
